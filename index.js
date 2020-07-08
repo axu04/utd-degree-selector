e9b26e49291c8d09be27a67771af89dbd02b68da
Alec Xu <alecxu@Alecs-MacBook-Pro-2.local>
2020-07-07 21:42:34 -0500

    change path of button

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ if (process.env.NODE_ENV === 'production') {
         app.use(express.static('client/courseplanner/build'))
 }
 
+app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 const courseRouter = require('./routes/courses')
 app.use('/courses', courseRouter)
 
