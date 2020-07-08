cbc77388b64d9bb1d40450e69e17396ffcbc5a45
Alec Xu <alecxu@Alecs-MacBook-Pro-2.local>
2020-07-07 21:29:17 -0500

    change path of button

--- client/courseplanner/src/api/api.js
+++ client/courseplanner/src/api/api.js
@@ -5,15 +5,15 @@ const api = axios.create({
         // baseURL: 'mongodb+srv://alecxu:Zsqwl58Ldo2prdHY@cluster0.xyl0w.mongodb.net/<dbname>?retryWrites=true&w=majority'
 })
 
-export const insertCourse = courseData => api.post('/courses', courseData)
-export const getAllCourses = () => api.get('/courses')
-export const deleteCourse = id => api.delete(`/courses/${id}`)
-export const getCourseById = id => api.get(`courses/${id}`)
+export const insertCourse = courseData => api.post('/api/courses', courseData)
+export const getAllCourses = () => api.get('/api/courses')
+export const deleteCourse = id => api.delete(`/api/courses/${id}`)
+export const getCourseById = id => api.get(`/apicourses/${id}`)
 
-export const insertDegree = degreeInfo => api.post('/degrees', degreeInfo)
-export const getAllDegrees = () => api.get('/degrees')
-export const deleteDegree = id => api.delete(`/degrees/${id}`)
-export const getDegreeById = id => api.get(`/degrees/${id}`)
+export const insertDegree = degreeInfo => api.post('/api/degrees', degreeInfo)
+export const getAllDegrees = () => api.get('/api/degrees')
+export const deleteDegree = id => api.delete(`/api/degrees/${id}`)
+export const getDegreeById = id => api.get(`/api/degrees/${id}`)
 
 const apis = {
         insertCourse,
